48192fee689ec1566b89f6240496b962ef28cfaa
Iuri de Silvio <iurisilvio@gmail.com>
2022-08-04 23:09:50 -0300

    Create randint_mock fixture.

--- opentelemetry_hook_sampler/tests.py
+++ opentelemetry_hook_sampler/tests.py
@@ -1,5 +1,6 @@
 from opentelemetry_hook_sampler import HookSampler
 from opentelemetry.sdk.trace.sampling import Decision, ParentBased
+import pytest
 
 
 def always_hook():
@@ -14,31 +15,34 @@ def partial_hook():
     return 20
 
 
-def test_always_hook(mocker):
-    randint_mock = mocker.patch("opentelemetry_hook_sampler.randint", autospec=True)
+@pytest.fixture
+def randint_mock(mocker):
+    return mocker.patch("opentelemetry_hook_sampler.randint", autospec=True)
+
+
+def test_always_hook(randint_mock):
     sampler = ParentBased(root=HookSampler(always_hook))
     result = sampler.should_sample(None, None, None)
     assert result.decision == Decision.RECORD_AND_SAMPLE
     randint_mock.assert_not_called()
 
 
-def test_never_hook(mocker):
-    randint_mock = mocker.patch("opentelemetry_hook_sampler.randint", autospec=True)
+def test_never_hook(randint_mock):
     sampler = ParentBased(root=HookSampler(never_hook))
     result = sampler.should_sample(None, None, None)
     assert result.decision == Decision.DROP
     randint_mock.assert_not_called()
 
 
-def test_partial_hook_sampled(mocker):
-    mocker.patch("opentelemetry_hook_sampler.randint", return_value=10)
+def test_partial_hook_sampled(randint_mock):
+    randint_mock.return_value = 10
     sampler = ParentBased(root=HookSampler(partial_hook))
     result = sampler.should_sample(None, None, None)
     assert result.decision == Decision.RECORD_AND_SAMPLE
 
 
-def test_partial_hook_dropped(mocker):
-    mocker.patch("opentelemetry_hook_sampler.randint", return_value=30)
+def test_partial_hook_dropped(randint_mock):
+    randint_mock.return_value = 30
     sampler = ParentBased(root=HookSampler(partial_hook))
     result = sampler.should_sample(None, None, None)
     assert result.decision == Decision.DROP
